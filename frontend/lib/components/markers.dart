0a1f73c95a8f48aca1df4295e02d2b13592e324e
Nawfal Cherkaoui Elmalki <nawfal.cherkaoui614@gmail.com>
2024-04-17 15:22:52 -0400

    Have initial draft of user location API

--- frontend/lib/components/markers.dart
+++ frontend/lib/components/markers.dart
@@ -1,5 +1,7 @@
 import "package:google_maps_flutter/google_maps_flutter.dart";
+import "package:geolocator/geolocator.dart";
 
 const ucfCoords = LatLng(28.6024, -81.2001);
-
 const sanFranCoords = LatLng(37.773972, -122.431297);
+
+
--- frontend/lib/components/navigation_ui.dart
+++ frontend/lib/components/navigation_ui.dart
@@ -1,14 +1,16 @@
 import "dart:ffi";
 
 import "package:flutter/material.dart";
-import "package:frontend/components/locations.dart";
+import "package:frontend/components/markers.dart";
 import "package:google_maps_flutter/google_maps_flutter.dart";
+import "package:geolocator/geolocator.dart";
 
 // TODO:
 // DONE - Figure out how to import this from env file
 // DONE - Center map over UCF
 // Enable custom locations
 // Enable following user location
+// Let team know to add center button to Figma
 // Enable point to point navigation
 // Create firebase with locations
 // Create API to get locations
@@ -21,15 +23,44 @@ class NavigationUI extends StatefulWidget {
 }
 
 class _NavigationUIState extends State<NavigationUI> {
-  late GoogleMapController mapController;
+  late GoogleMapController _mapController;
+  // late LatLng _userLocation;
 
-  static const _initialCameraPosition = CameraPosition(
-    target: sanFranCoords,
+  static var _initialCameraPosition = const CameraPosition(
+    target: ucfCoords,
     zoom: 18.0,
   );
 
   void _onMapCreated(GoogleMapController controller) {
-    mapController = controller;
+    _mapController = controller;
+    _getCurrentLocation().then((value) => {
+      _initialCameraPosition = CameraPosition(target: LatLng(value.latitude, value.longitude), zoom: 18.0)
+    });
+    _mapController.animateCamera(
+          CameraUpdate.newCameraPosition(_initialCameraPosition),
+    );
+  }
+
+  Future<Position> _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error('Location permissions are permanently denied, we cannot request your location');
+    }
+
+    return await Geolocator.getCurrentPosition();
   }
 
   @override
@@ -47,7 +78,7 @@ class _NavigationUIState extends State<NavigationUI> {
       floatingActionButton: FloatingActionButton(
         backgroundColor: Theme.of(context).primaryColor,
         foregroundColor: Colors.black,
-        onPressed: () => mapController.animateCamera(
+        onPressed: () => _mapController.animateCamera(
           CameraUpdate.newCameraPosition(_initialCameraPosition),
         ),
         child: const Icon(Icons.center_focus_strong),

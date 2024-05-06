ea58dc5af114ba50b84fbc853a0201c59f549e4f
Nawfal Cherkaoui Elmalki <nawfal.cherkaoui614@gmail.com>
2024-05-06 11:08:49 -0400

    Added .env support for android and IOS to store Google Maps API key

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_config/flutter_config.dart';
 import 'package:frontend/pages/venture_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await FlutterConfig.loadEnvVariables();
+
   runApp(const MaterialApp(
     home: VentureScreen(),
   ));

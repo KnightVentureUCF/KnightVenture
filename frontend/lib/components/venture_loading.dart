e0ff82f1d18d0783f17bf8873c66f28c97aa54c8
Nawfal Cherkaoui Elmalki <nawfal.cherkaoui614@gmail.com>
2024-05-12 11:53:51 -0400

    Setup layout for api integration test

---
+++ frontend/lib/components/venture_loading.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class VentureLoading extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: CircularProgressIndicator());
+  }
+}

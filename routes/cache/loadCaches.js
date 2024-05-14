d1b9c5eac57d44771f894dfd4b8f0caf902de4a4
Nawfal Cherkaoui Elmalki <nawfal.cherkaoui614@gmail.com>
2024-05-14 12:47:45 -0400

    Set up in-progress readCache API

--- routes/cache/loadCaches.js
+++ routes/cache/loadCaches.js
@@ -5,15 +5,14 @@ const db = admin.firestore();
 const collectionRef = db.collection('Caches');
 const {verifyAccessTokenMiddleware} = require("../../utils/verifyAccessTokenMiddleware.js");
 
-// Define a route to get data from the Firestore database
-router.get('/load_caches', verifyAccessTokenMiddleware, async (req, res) => {
+// Route for loading all UCF caches for the venture screen.
+router.post('/load_caches', verifyAccessTokenMiddleware, async (req, res) => {
   try {
-    // Now you can access req.userData here if you need to use it
     const snapshot = await collectionRef.get();
     const data = snapshot.docs.map(doc => doc.data());
     res.status(200).send({
       caches: data,
-      message: "Loaded Caches Successfully"
+      message: "Loaded Caches Successfully."
     });
   } catch (error) {
     res.status(500).send({
--- routes/cache/readCache.test.js
+++ routes/cache/readCache.test.js
@@ -3,10 +3,12 @@ const app = require('../../app.js');
 require('dotenv').config();
 const describeIfTest = process.env.ENVIRONMENT === 'test' ? describe : describe.skip;
 
-describeIfTest('POST /api/load_caches', () => {
+describeIfTest('POST /api/read_cache', () => {
     it('should connect to the server', async () => {
         expect.assertions(1);
-        const response = await supertest(app).post("/api/load_caches").send();
-        expect([200, 500]).toContain(response.statusCode);
+        const response = await supertest(app).post("/api/read_cache").send({
+            cacheID: "test"
+        });
+        expect([200, 404, 500]).toContain(response.statusCode);
     });
 })

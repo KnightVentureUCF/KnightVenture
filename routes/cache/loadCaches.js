9deeecd3cf12acfa17edafcd2496f955d2ad4a20
Nawfal Cherkaoui Elmalki <nawfal.cherkaoui614@gmail.com>
2024-05-14 10:29:54 -0400

    Working skeleton

--- routes/cache/loadCaches.js
+++ routes/cache/loadCaches.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const admin = require('firebase-admin');
-
 const router = express.Router();
 const db = admin.firestore();
+
 const collectionRef = db.collection('Caches');
 
 // Define a route to get data from the Firestore database

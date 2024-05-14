2e6bcfb3d6f655e77841aa65de0f1dfa2683315f
Nawfal Cherkaoui Elmalki <nawfal.cherkaoui614@gmail.com>
2024-05-14 10:34:13 -0400

    Added in-progress unit tests

--- backend/cache/loadCaches.test.js
+++ backend/cache/loadCaches.test.js
@@ -3,8 +3,9 @@ const app = require('../../app.js');
 
 describe('GET /api/load_caches', () => {
     it('should have status 200', async () => {
-        expect.assertions(1);
+        expect.assertions(2);
         const response = await supertest(app).get("/api/load_caches").send();
         expect(response.statusCode).toBe(200);
+        expect(response.body.caches).toEqual([]);
     });
 })

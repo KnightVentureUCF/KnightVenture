a3b50c44cded021919c8b7388358f8966970588c
Hong Thy Nguyen <hongthy3101@gmail.com>
2024-05-14 22:03:43 -0400

    updated login screen

--- frontend/lib/widgets/login/login_screen.dart
+++ frontend/lib/widgets/login/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'login_widget.dart';
 import 'signup_widget.dart';
 
@@ -11,20 +13,19 @@ class _LoginScreenState extends State<LoginScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.yellow[700],
+      backgroundColor: Color(0xffFFC904),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Padding(
-              padding: const EdgeInsets.only(bottom: 20.0),
-              child: Icon(
-                Icons.shield,
-                size: 100.0,
-                color: Colors.black,
-              ),
-            ),
-            Text(
+                padding: const EdgeInsets.only(bottom: 20.0),
+                child: Image.asset(
+                  'assets/knight_icon.png',
+                  width: 200,
+                  height: 200,
+                )),
+            const Text(
               'KnightVenture',
               style: TextStyle(
                 fontWeight: FontWeight.bold,
@@ -32,8 +33,8 @@ class _LoginScreenState extends State<LoginScreen> {
                 color: Colors.black,
               ),
             ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
+            const Padding(
+              padding: EdgeInsets.all(8.0),
               child: Text(
                 "Knight cannot be made to fear. Let’s the hunt begin!",
                 textAlign: TextAlign.center,
@@ -41,28 +42,41 @@ class _LoginScreenState extends State<LoginScreen> {
               ),
             ),
             SizedBox(height: 50),
-            ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.black,
-                foregroundColor: Colors.white,
+            SizedBox(
+              width: 305,
+              height: 61,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                ),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => LoginWidget()),
+                ),
+                child: Text('Login'),
               ),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => LoginWidget()),
-              ),
-              child: Text('Login'),
             ),
             SizedBox(height: 20),
-            OutlinedButton(
-              style: OutlinedButton.styleFrom(
-                side: BorderSide(color: Colors.black),
+            SizedBox(
+              width: 305,
+              height: 61,
+              child: OutlinedButton(
+                style: OutlinedButton.styleFrom(
+                  side: BorderSide(color: Colors.black),
+                ),
+                onPressed: () => Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SignUpWidget()),
+                ),
+                child: Text('Sign Up', style: TextStyle(color: Colors.black)),
               ),
-              onPressed: () => Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => SignUpWidget()),
-              ),
-              child: Text('Sign Up', style: TextStyle(color: Colors.black)),
             ),
+            const Image(
+              image: AssetImage('assets/horseback.png'),
+              width: 100,
+              height: 100,
+            )
           ],
         ),
       ),

67a35c8c713ff891754ef37ce072e684e8ee8810
Hong Thy Nguyen <hongthy3101@gmail.com>
2024-08-21 14:33:19 -0400

    resolving merge issues

--- frontend/macos/Flutter/GeneratedPluginRegistrant.swift
+++ frontend/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,8 +5,8 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_foundation
+import geolocator_apple
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
-  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  GeolocatorPlugin.register(with: registry.registrar(forPlugin: "GeolocatorPlugin"))
 }

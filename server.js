ee58dbb5f2397a8a841b1851e9ce30a72fc06d5a
omar <omaralshafei02@gmail.com>
2024-08-21 20:14:44 -0400

    debugging heroku errors

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const client = new AWS.CognitoIdentityServiceProvider({
 
 // Confiure Firebase
 const admin = require('firebase-admin');
-const serviceAccount = require('./firebasePrivateKey.json');
+const serviceAccount = JSON.parse(process.env.FIREBASE_CREDENTIALS);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DATABASE_URL,

1e1deb083444c54dba3668092fa67f3a4dc46707
Omar Alshafei <omaralshafei02@gmail.com>
2024-09-04 17:37:25 -0400

    bug fix

--- server.js
+++ server.js
@@ -17,24 +17,14 @@ const client = new AWS.CognitoIdentityServiceProvider({
   region: process.env.REGION,
 });
 
-// Confiure Firebase
+// Configure Firebase
 const admin = require('firebase-admin');
-const serviceAccount = {
-  type: process.env.FIREBASE_TYPE,
-  project_id: process.env.FIREBASE_PROJECT_ID,
-  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  private_key: process.env.FIREBASE_PRIVATE_KEY,
-  client_email: process.env.FIREBASE_CLIENT_EMAIL,
-  client_id: process.env.FIREBASE_CLIENT_ID,
-  auth_uri: process.env.FIREBASE_AUTH_URI,
-  token_uri: process.env.FIREBASE_TOKEN_URI,
-  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
-  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
-};
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY);
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
   databaseURL: process.env.FIREBASE_DATABASE_URL,
 });
+
 // const dtb = admin.firestore();
 
 // Middleware to parse JSON bodies

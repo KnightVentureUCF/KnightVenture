0cf91bd992237cad80800adad79cf40cfe7aaa0e
Omar Alshafei <omaralshafei02@gmail.com>
2024-09-04 17:16:05 -0400

    still debugging

--- server.js
+++ server.js
@@ -25,8 +25,8 @@ const serviceAccount = {
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
   // Replace \\n with actual \n and remove surrounding quotes
   private_key: process.env.FIREBASE_PRIVATE_KEY
-    ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n").slice(1, -1)
-    : undefined,
+  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
+  : undefined,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,

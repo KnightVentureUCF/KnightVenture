e17dc9f2e69ad31c597bcc8e4aec5051be6554a3
Omar Alshafei <omaralshafei02@gmail.com>
2024-09-04 17:22:34 -0400

    hopefully this will fix it

--- server.js
+++ server.js
@@ -23,10 +23,7 @@ const serviceAccount = {
   type: process.env.FIREBASE_TYPE,
   project_id: process.env.FIREBASE_PROJECT_ID,
   private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
-  // Replace \\n with actual \n and remove surrounding quotes
-  private_key: process.env.FIREBASE_PRIVATE_KEY
-  ? process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, "\n")
-  : undefined,
+  private_key: process.env.FIREBASE_PRIVATE_KEY,
   client_email: process.env.FIREBASE_CLIENT_EMAIL,
   client_id: process.env.FIREBASE_CLIENT_ID,
   auth_uri: process.env.FIREBASE_AUTH_URI,
@@ -78,7 +75,7 @@ const readCache = require('./backend/routes/cache/readCache');
 app.use('/api/read_cache', readCache);
 
 // Listen on a port
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
 

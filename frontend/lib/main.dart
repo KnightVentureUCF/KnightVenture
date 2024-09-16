85966352d736d761e33991475e121a74c3949ed5
Hong Thy Nguyen <hongthy3101@gmail.com>
2024-09-16 17:01:39 -0400

    added forgot username widget

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/widgets/credentials/forgot_username_screen.dart';
 import 'package:frontend/widgets/credentials/reset_new_password.dart';
 import 'package:frontend/widgets/credentials/credentials_screen.dart';
 import 'package:frontend/widgets/credentials/forgot_password_screen.dart';
@@ -6,7 +7,7 @@ import 'package:frontend/widgets/credentials/forgot_something_screen.dart';
 import 'package:frontend/widgets/credentials/loading_screen.dart';
 
 void main() {
-  runApp(const MaterialApp(
-    home: LoadingScreen(),
+  runApp(MaterialApp(
+    home: ForgotUsernameWidget(),
   ));
 }

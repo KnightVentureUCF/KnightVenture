866baa7a964e4aee13866808381172660e9e42dd
Omar Alshafei <104457916+OmarAlshafei@users.noreply.github.com>
2024-09-20 18:05:23 -0400

    updated verifyAccessTokenMiddleware.js

--- backend/utils/verifyAccessTokenMiddleware.js
+++ backend/utils/verifyAccessTokenMiddleware.js
@@ -1,7 +1,6 @@
 const { verifyAccessToken } = require("../utils/cognito-utils");
 // Middleware to verify the access token
 async function verifyAccessTokenMiddleware(req, res, next) {
-    if (process.env.ENVIRONMENT !== "test") {
       const { accessToken } = req.body;
       if (!accessToken) {
         return res.status(400).send({ message: 'Invalid request.' });
@@ -14,7 +13,7 @@ async function verifyAccessTokenMiddleware(req, res, next) {
   
       // Attach userData to the request object so it can be used in subsequent routes
       req.userData = userData;
-    }
+    
     next(); // Proceed to the next middleware or route handler
   }
 

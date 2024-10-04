9a5b8c73806fda6e9b5fb91c28f88143a95a4db2
Eduardo Alejandro Vila <35271827+SpaghettiEddy@users.noreply.github.com>
2024-10-03 22:40:57 -0400

    blah blah

--- frontend/lib/widgets/home/home_screen.dart
+++ frontend/lib/widgets/home/home_screen.dart
@@ -7,15 +7,9 @@ import 'package:frontend/data/all_caches.dart';
 import 'package:frontend/widgets/home/cache_popup.dart';
 
 class HomeScreen extends StatelessWidget {
-<<<<<<< HEAD
-  final TestCache myCache = allCaches[0];
-
-  HomeScreen({super.key});
-=======
   final String accessToken;
 
   const HomeScreen({Key? key, required this.accessToken}) : super(key: key);
->>>>>>> origin
 
   @override
   Widget build(BuildContext context) {
@@ -38,21 +32,6 @@ class HomeScreen extends StatelessWidget {
                 Icons.menu,
                 size: 48,
                 color: Colors.black,
-              ),
-            ),
-          ),
-          Positioned(
-            bottom: 45,
-            left: 0,
-            right: 0,
-            child: GestureDetector(
-              onTap: () {
-                CachePopup.show(context, myCache);
-              },
-              child: Image.asset(
-                'assets/logo.png', // Replace with your actual path
-                width: 160, // Set the desired width
-                height: 160,
               ),
             ),
           ),

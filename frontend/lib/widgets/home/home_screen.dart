92186023d9c3b1ea23687c2fb5facbec33480a1d
Eduardo Alejandro Vila <35271827+SpaghettiEddy@users.noreply.github.com>
2024-10-07 21:25:56 -0400

    should now find nearest cache

--- frontend/lib/widgets/home/home_screen.dart
+++ frontend/lib/widgets/home/home_screen.dart
@@ -3,8 +3,6 @@ import 'package:frontend/widgets/home/navigation_ui.dart';
 import 'package:frontend/widgets/styling/theme.dart';
 import 'package:frontend/widgets/main_menu/main_menu_screen.dart';
 import 'package:frontend/models/test_cache.dart';
-import 'package:frontend/data/all_caches.dart';
-import 'package:frontend/widgets/home/cache_popup.dart';
 
 class HomeScreen extends StatelessWidget {
   final String accessToken;
@@ -17,6 +15,24 @@ class HomeScreen extends StatelessWidget {
       body: Stack(
         children: [
           NavigationUI(accessToken: accessToken),
+          Positioned(
+            top: 75,
+            right: 35,
+            child: GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                      builder: (context) => const MainMenuScreen()),
+                );
+              },
+              child: const Icon(
+                Icons.menu,
+                size: 48,
+                color: Colors.black,
+              ),
+            ),
+          ),
         ],
       ),
     );

9c5d2961b02b40b497ea5e909e6de540eb93b2dc
Omar Alshafei <omaralshafei02@gmail.com>
2024-10-11 21:46:37 -0400

    customized pins

--- frontend/lib/widgets/home/navigation_ui.dart
+++ frontend/lib/widgets/home/navigation_ui.dart
@@ -1,6 +1,10 @@
 import "package:flutter/material.dart";
+import "package:frontend/widgets/home/cache_popup.dart";
 import "package:frontend/widgets/home/loading_screen.dart";
+import "package:frontend/widgets/home/venture_button.dart";
 import "package:google_maps_flutter/google_maps_flutter.dart";
+import 'package:frontend/widgets/main_menu/main_menu_screen.dart';
+import 'package:frontend/data/all_caches.dart';
 import "package:geolocator/geolocator.dart";
 import "package:frontend/models/caches.dart" as caches;
 import "package:frontend/constants.dart" show initialMapZoomOnVentureScreen;
@@ -17,11 +21,18 @@ class NavigationUI extends StatefulWidget {
 
 class _NavigationUIState extends State<NavigationUI> {
   late GoogleMapController _mapController;
+
+  // Used to load and store user and cache locations
   late LatLng _currentLocation;
-  LatLng? _destination;
   bool _cacheLocationsLoaded = false;
   bool _userLocationLoaded = false;
   final Map<String, Marker> _cacheMarkers = {};
+  List<caches.Cache> _allCaches = [];
+
+  // Variables for cache navigation and quiz popup
+  caches.Cache? _destination;
+  bool _reachedDestination = false;
+  static const double reachedDestinationThreshold = 10;
 
   // Define UCF location data
   bool _userLocatedAtUCF = false;
@@ -39,32 +50,24 @@ class _NavigationUIState extends State<NavigationUI> {
   }
 
 void _loadCacheMarkers() async {
-  final BitmapDescriptor knightIcon = await BitmapDescriptor.fromAssetImage(
+  final cacheLocations = await caches.getCacheLocations(widget.accessToken);
+  allCaches = cacheLocations;
+
+  final BitmapDescriptor customIcon = await BitmapDescriptor.fromAssetImage(
     const ImageConfiguration(size: Size(48, 48)),
     'assets/knight_icon_small.png',
   );
 
-  final cacheLocations = await caches.getCacheLocations(widget.accessToken);
-
   setState(() {
     _cacheMarkers.clear();
+    _allCaches = cacheLocations.caches;
     for (final cache in cacheLocations.caches) {
       final coords = LatLng(cache.lat, cache.lng);
       final marker = Marker(
         markerId: MarkerId(cache.name),
         position: coords,
-        icon: knightIcon,
-        onTap: () {
-          if (_userLocatedAtUCF == true && _destination != coords) {
-            setState(() {
-              _destination = coords;
-            });
-          } else if (_destination == coords) {
-            setState(() {
-              _destination = null;
-            });
-          }
-        },
+        icon: customIcon,
+        onTap: () => beginCacheNavigation(_userLocatedAtUCF, cache),
         infoWindow: InfoWindow(
           title: cache.name,
         ),
@@ -74,6 +77,18 @@ void _loadCacheMarkers() async {
     _cacheLocationsLoaded = true;
   });
 }
+
+  void beginCacheNavigation(bool userLocatedAtUCF, caches.Cache cache) {
+    if (_userLocatedAtUCF == true && _destination != cache) {
+      setState(() {
+        _destination = cache;
+      });
+    } else if (_destination == cache) {
+      setState(() {
+        _destination = null;
+      });
+    }
+  }
 
   bool userAtUCF(double userLat, double userLng) {
     double distanceInMeters = Geolocator.distanceBetween(
@@ -86,15 +101,25 @@ void _loadCacheMarkers() async {
   void _updateCameraPosition(double lat, double lng) {
     var userLocation = LatLng(lat, lng);
     var userInUCF = userAtUCF(lat, lng);
+    var reachedDestination = false;
 
     if (userInUCF == false) {
       userLocation = ucfCampusCenter;
+    }
+    if (_destination != null) {
+      double distanceToCache = Geolocator.distanceBetween(
+          _currentLocation.latitude,
+          _currentLocation.longitude,
+          _destination!.lat,
+          _destination!.lng);
+
+      reachedDestination = distanceToCache < reachedDestinationThreshold;
     }
 
     setState(() {
       _currentLocation = userLocation;
       _userLocatedAtUCF = userInUCF;
-      _userLocationLoaded = true;
+      _reachedDestination = reachedDestination;
     });
   }
 
@@ -103,77 +128,82 @@ void _loadCacheMarkers() async {
   }
 
 // Asks for the user's location and returns error if unavailable.
-void _getCurrentLocation() async {
-  bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
-  if (!serviceEnabled) {
-    _showErrorDialog(
-      context,
-      "Location Services Disabled",
-      "Location services are disabled. Please enable them in settings.",
-    );
-    return Future.error('Location services are disabled.');
-  }
-
-  LocationPermission permission = await Geolocator.checkPermission();
-
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
-    if (permission == LocationPermission.denied) {
+  void _getCurrentLocation() async {
+    bool serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
       _showErrorDialog(
         context,
-        "Location Permission Denied",
-        "Location permissions are denied. Please enable them in settings.",
+        "Location Services Disabled",
+        "Location services are disabled. Please enable them in settings.",
       );
-      return Future.error('Location permissions are denied');
+      return Future.error('Location services are disabled.');
     }
+
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        _showErrorDialog(
+          context,
+          "Location Permission Denied",
+          "Location permissions are denied. Please enable them in settings.",
+        );
+        return Future.error('Location permissions are denied');
+      }
+    }
+
+    if (permission == LocationPermission.deniedForever) {
+      // Show dialog that explains the user needs to enable permissions manually
+      _showErrorDialog(
+        context,
+        "Location Permission Permanently Denied",
+        "Location permissions are permanently denied. Please enable them in settings.",
+        openAppSettings: true, // Pass flag to open settings if needed
+      );
+      return Future.error(
+          'Location permissions are permanently denied, we cannot request your location');
+    }
+
+    // Get the user's current location
+    var position = await Geolocator.getCurrentPosition();
+    setState(() {
+      _userLocationLoaded = true;
+    });
+
+    _updateCameraPosition(position.latitude, position.longitude);
+
+    _updateLiveLocation();
   }
-
-  if (permission == LocationPermission.deniedForever) {
-    // Show dialog that explains the user needs to enable permissions manually
-    _showErrorDialog(
-      context,
-      "Location Permission Permanently Denied",
-      "Location permissions are permanently denied. Please enable them in settings.",
-      openAppSettings: true, // Pass flag to open settings if needed
-    );
-    return Future.error(
-        'Location permissions are permanently denied, we cannot request your location');
-  }
-
-  // Get the user's current location
-  var position = await Geolocator.getCurrentPosition();
-
-  _updateCameraPosition(position.latitude, position.longitude);
-
-  _updateLiveLocation();
-}
 
 // Show a dialog to handle location permission issues
-void _showErrorDialog(BuildContext context, String title, String message, {bool openAppSettings = false}) {
-  showDialog(
-    context: context,
-    builder: (BuildContext context) {
-      return AlertDialog(
-        title: Text(title),
-        content: Text(message),
-        actions: <Widget>[
-          if (openAppSettings)
+  void _showErrorDialog(BuildContext context, String title, String message,
+      {bool openAppSettings = false}) {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text(title),
+          content: Text(message),
+          actions: <Widget>[
+            if (openAppSettings)
+              TextButton(
+                onPressed: () {
+                  Geolocator
+                      .openAppSettings(); // Open app settings via Geolocator
+                  Navigator.of(context).pop();
+                },
+                child: const Text("Open Settings"),
+              ),
             TextButton(
-              onPressed: () {
-                Geolocator.openAppSettings(); // Open app settings via Geolocator
-                Navigator.of(context).pop();
-              },
-              child: const Text("Open Settings"),
+              child: const Text("OK"),
+              onPressed: () => Navigator.of(context).pop(),
             ),
-          TextButton(
-            child: const Text("OK"),
-            onPressed: () => Navigator.of(context).pop(),
-          ),
-        ],
-      );
-    },
-  );
-}
+          ],
+        );
+      },
+    );
+  }
 
   void _updateLiveLocation() {
     LocationSettings locationSettings = const LocationSettings(
@@ -202,7 +232,10 @@ void _showErrorDialog(BuildContext context, String title, String message, {bool 
           ? {
               Polyline(
                 polylineId: const PolylineId("route"),
-                points: [_currentLocation, _destination!],
+                points: [
+                  _currentLocation,
+                  LatLng(_destination!.lat, _destination!.lng)
+                ],
                 color: Colors.blue,
                 width: 6,
               ),
@@ -244,6 +277,17 @@ void _showErrorDialog(BuildContext context, String title, String message, {bool 
               child: const Icon(Icons.my_location),
             ),
           ),
+          _destination == null
+              ? VentureButton(
+                  allCaches: _allCaches,
+                  currentLocation: _currentLocation,
+                  beginCacheNavigation: beginCacheNavigation,
+                  userLocatedAtUCF: _userLocatedAtUCF,
+                )
+              : const SizedBox.shrink(),
+          _destination != null && _reachedDestination
+              ? CachePopup(cache: _destination!)
+              : const SizedBox.shrink()
         ],
       );
     }

881f2d1fd4efdae6f451fd67e243f952b7e88cc5
Hong Thy Nguyen <hongthy3101@gmail.com>
2024-11-10 22:36:37 -0500

    display errors if the users register fail in register page

--- backend/routes/user/register.js
+++ backend/routes/user/register.js
@@ -1,3 +1,42 @@
+// const express = require('express');
+// const router = express.Router();
+// const dotenv = require('dotenv');
+// const path = require('path');
+// const AWS = require('aws-sdk');
+
+// // Configure AWS using environment variables
+// AWS.config.update({
+//   region: process.env.REGION,
+// });
+
+// // Create Cognito Client
+// const client = new AWS.CognitoIdentityServiceProvider({
+//   apiVersion: '2024-05-06',
+//   region: process.env.REGION,
+// });
+
+// router.post('/', (req, res) => {
+//   const { username, email, password } = req.body;
+//   const params = {
+//     ClientId: process.env.CLIENT_ID,
+//     Password: password,
+//     Username: username,
+//     UserAttributes: [
+//       {
+//         Name: 'email',
+//         Value: email,
+//       },
+//     ],
+//   };
+
+//   client.signUp(params, (err, data) => {
+//     if (err) res.status(500).send(err);
+//     else res.send(data);
+//   });
+// });
+
+// module.exports = router;
+
 const express = require('express');
 const router = express.Router();
 const dotenv = require('dotenv');
@@ -30,8 +69,24 @@ router.post('/', (req, res) => {
   };
 
   client.signUp(params, (err, data) => {
-    if (err) res.status(500).send(err);
-    else res.send(data);
+    if (err) {
+      // Handle only UsernameExistsException and InvalidParameterException
+      if (err.code === 'UsernameExistsException') {
+        res.status(409).json({ error: 'Username already exists.' });
+      } else if (err.code === 'InvalidParameterException') {
+        res.status(400).json({
+          error:
+            'Invalid input parameters. Please check username, email, and password.',
+        });
+      } else {
+        res.status(500).json({
+          error: 'An error occurred during sign-up.',
+          details: err.message,
+        });
+      }
+    } else {
+      res.send(data);
+    }
   });
 });
 
